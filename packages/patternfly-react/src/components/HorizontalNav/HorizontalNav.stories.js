18dac447c2da5c44c854ffa04c7b13195562c9a8
Alberto Gutierrez <aljesusg@gmail.com>
2018-06-20 18:54:09 +0200

    f

--- packages/patternfly-react/src/components/HorizontalNav/HorizontalNav.stories.js
+++ packages/patternfly-react/src/components/HorizontalNav/HorizontalNav.stories.js
@@ -7,20 +7,16 @@ import {
     DOCUMENTATION_URL,
     STORYBOOK_CATEGORY
 } from 'storybook/constants/siteConstants';
-import { action } from '@storybook/addon-actions';
-import { HorizontalNav, HorizontalNavHeader, HorizontalCollapse, VerticalNavBrand, ListGroup, ListGroupItem, DropdownButton, MenuItem, Icon } from '../../index';
+
+import { Masthead, ListGroup, ListGroupItem, MenuItem } from '../../index';
 
 
 import { name } from '../../../package.json';
 
 
-const title1 = <Icon type="pf" name="help" title="Help" />;
-const title2 = (
-    <span>
-    <Icon type="pf" name="user" style={{ marginRight: 10 + 'px' }} />
-    Brian Johnson
-  </span>
-);
+import pfLogo from 'storybook/img/logo-alt.svg';
+import pfBrand from 'storybook/img/brand-alt.svg';
+
 const stories = storiesOf(
     `${storybookPackageName(name)}/${
         STORYBOOK_CATEGORY.NAVIGATION
@@ -41,59 +37,44 @@ stories.addDecorator(
 
 
 stories.addWithInfo('Horizontal Navigation', '', () => (
-    <HorizontalNav>
-        <HorizontalNavHeader>
-            <VerticalNavBrand
-                href="/"
-                iconImg="http://www.patternfly.org/assets/img/brand.svg"
-            />
-        </HorizontalNavHeader>
-        <HorizontalCollapse>
-            <ListGroup bsClass="nav navbar-nav navbar-utility">
-                <ListGroupItem bsClass="">
-                    <DropdownButton
-                        title={title1}
-                        id="help_button"
-                        onClick={action('onClick')}
-                    >
-                        <MenuItem bsClass="" eventKey="1">
-                            Help
-                        </MenuItem>
-                        <MenuItem bsClass="" eventKey="2">
-                            About
-                        </MenuItem>
-                    </DropdownButton>
-                </ListGroupItem>
-                <ListGroupItem bsClass="dropdown">
-                    <DropdownButton
-                        title={title2}
-                        id="user_button"
-                        onClick={action('onClick')}
-
-                    >
-                        <MenuItem bsClass="" eventKey="1">
-                            Link
-                        </MenuItem>
-                        <MenuItem bsClass="" eventKey="2">
-                            Another link
-                        </MenuItem>
-                        <MenuItem bsClass="" eventKey="2">
-                            Something else here
-                        </MenuItem>
-                    </DropdownButton>
-                </ListGroupItem>
-            </ListGroup>
-            <ListGroup bsClass="nav navbar-nav navbar-primary">
-                <ListGroupItem bsClass="">
-                    <a href="#0">First Link</a>
-                </ListGroupItem>
-                <ListGroupItem bsClass="" active>
-                    <a href="#0">Another Link</a>
-                </ListGroupItem>
-                <ListGroupItem bsClass="">
-                    <a href="#0">And Another</a>
-                </ListGroupItem>
-            </ListGroup>
-        </HorizontalCollapse>
-    </HorizontalNav>
+    <div>
+        <Masthead
+            iconImg={pfLogo}
+            titleImg={pfBrand}
+            title="Patternfly React"
+        >
+            <Masthead.Collapse>
+                <Masthead.Dropdown
+                    id="app-help-dropdown"
+                    title={<span title="Help" className="pficon pficon-help" />}
+                >
+                    <MenuItem eventKey="1">Help</MenuItem>
+                    <MenuItem eventKey="2">About</MenuItem>
+                </Masthead.Dropdown>
+                <Masthead.Dropdown
+                    id="app-user-dropdown"
+                    title={
+                        <span>
+              <span title="Help" className="pficon pficon-user" />
+              <span className="dropdown-title"> Brian Johnson</span>
+            </span>
+                    }
+                >
+                    <MenuItem eventKey="1">User Preferences</MenuItem>
+                    <MenuItem eventKey="2">Logout</MenuItem>
+                </Masthead.Dropdown>
+            </Masthead.Collapse>
+        </Masthead>
+        <ListGroup bsClass="nav navbar-nav navbar-primary">
+            <ListGroupItem bsClass="">
+                <a href="#0">First Link</a>
+            </ListGroupItem>
+            <ListGroupItem bsClass="" active>
+                <a href="#0">Another Link</a>
+            </ListGroupItem>
+            <ListGroupItem bsClass="">
+                <a href="#0">And Another</a>
+            </ListGroupItem>
+        </ListGroup>
+    </div>
 ));
